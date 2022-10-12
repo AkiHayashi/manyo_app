392727c08111dee66bfc65f99d81ab1d0aaa25b4
aki hayashi <hayashi1019toy@gmail.com>
2022-10-13 02:05:42 +0900

    feat: 🎸 add FactoryBot and fix gem defined location

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,8 +19,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '登録済みのタスク一覧が表示される' do
         # テストで使用するためのタスクを登録
-        Task.create!(title: '書類作成', content: '企画書を作成する。')
-        # タスク一覧画面に遷移
+        FactoryBot.create(:task)
         visit tasks_path
         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
         expect(page).to have_content '書類作成'
@@ -32,10 +31,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it 'そのタスクの内容が表示される' do
-        task = Task.create!(title: '書類作成', content: '企画書を作成する。')
+        task = FactoryBot.create(:second_task, title: '競合調査', content: '他社のサービスを調査する。')
         visit task_path(task)
-        expect(page).to have_content '書類作成'
-        expect(page).to have_content '企画書を作成する。'
+        expect(page).to have_content '競合調査'
+        expect(page).to have_content '他社のサービスを調査する。'
       end
     end
   end
