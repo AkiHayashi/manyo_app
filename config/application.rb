e411bab1b3ede6f87f5d77e4bcbb20f7f3d09592
aki hayashi <hayashi1019toy@gmail.com>
2022-10-14 20:54:39 +0900

    タイムゾーンをJapan/Tokyoに設定

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module CdpWebManyoTask
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.generators do |g|
       g.assets false
       g.helper false
