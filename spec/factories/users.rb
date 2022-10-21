287a343cd126112890e48e3774095c4c0ec69969
aki hayashi <hayashi1019toy@gmail.com>
2022-10-22 01:54:29 +0900

    add test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    name { 'aki' }
+    email { 'aki@ex.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+    admin { false }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'ユーザモデル機能', type: :model do
+  describe 'バリデーションのテスト' do
+    context 'ユーザの名前が空文字の場合' do
+      it 'バリデーションに失敗する' do
+        user = User.create(name: "",
+                           email: "aki@ex.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのメールアドレスが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        user = User.create(name: "aki",
+                           email: "",
+                           password: "password",
+                           password_confirmation: "password",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのパスワードが空文字の場合' do
+      it 'バリデーションに失敗する' do
+        user = User.create(name: "aki",
+                           email: "aki@ex.com",
+                           password: "",
+                           password_confirmation: "password",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのメールアドレスがすでに使用されていた場合' do
+      it 'バリデーションに失敗する' do
+        FactoryBot.create(:user, email: "aki@ex.com")
+        user = User.create(name: "aki",
+                           email: "aki@ex.com",
+                           password: "password",
+                           password_confirmation: "password",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザのパスワードが6文字未満の場合' do
+      it 'バリデーションに失敗する' do
+        user = User.create(name: "aki",
+                           email: "aki@ex.com",
+                           password: "1",
+                           password_confirmation: "1",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'ユーザの名前に値があり、メールアドレスが使われていない値で、かつパスワードが6文字以上の場合' do
+      it 'バリデーションに成功する' do
+        user = User.create(name: "aki",
+                           email: "aki@ex.com",
+                           password: "password1",
+                           password_confirmation: "password",
+                           admin: true)
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
