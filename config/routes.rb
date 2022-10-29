4bcc5c78a8d471105405cb5583a6d50d96282d19
aki hayashi <hayashi1019toy@gmail.com>
2022-10-29 15:19:32 +0900

    create Label CRUD function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   resources :tasks
+  resources :labels, only: %i[new create edit update index destroy]
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create show edit update]
   namespace :admin do
